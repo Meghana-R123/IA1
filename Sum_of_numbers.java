7438d561270db2efec07d12bb73802399bbc9633
MEGHANA R <meghuram2005@gmail.com>
2026-01-30 09:48:27 +0530

    Create Sum_of_numbers.java

---
+++ Sum_of_numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Sum_of_numbers {
+  public static void main(String[] args){
+    int a=10;
+    int b=20;
+    int c=30;
+    int sum=a+b+c;
+    System.out.println("Sum is :"+sum)
+  }
+}
